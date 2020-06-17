eba61a727a80824ebd3d48aba04e45c989c348bc
Wilfred0zz <wilfredo.recinos52@myhunter.cuny.edu>
2020-06-17 18:10:14 -0400

    Updates

---
+++ precache-manifest.d1f8864a604cd7e0134cdf6dc786ada4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2170fc1176d4a0aa3c7dfdf36c5500",
+    "url": "/React-Grid-Paint/index.html"
+  },
+  {
+    "revision": "6df669e0f4b082c36558",
+    "url": "/React-Grid-Paint/static/css/main.f2d2637f.chunk.css"
+  },
+  {
+    "revision": "2810c88c8ed4cefdadaf",
+    "url": "/React-Grid-Paint/static/js/2.7b5d247c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Grid-Paint/static/js/2.7b5d247c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df669e0f4b082c36558",
+    "url": "/React-Grid-Paint/static/js/main.66f8fa16.chunk.js"
+  },
+  {
+    "revision": "9668b0d533ab403e5b66",
+    "url": "/React-Grid-Paint/static/js/runtime-main.1438dd4a.js"
+  }
+]);
